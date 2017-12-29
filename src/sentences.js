af31329b5165b8073004e30e53502f9f0f0df2d0
Niwat Charoenloet <kenzonico@gmail.com>
2017-12-28 23:20:25 -0800

    Changed sentence extraction function for a more reliable one.

--- src/sentences.js
+++ src/sentences.js
@@ -1,4 +1,5 @@
-var natural = require('natural')
+const natural = require('natural')
+const nlp = require('compromise')
 
 const Sentences = {}
 
@@ -9,6 +10,16 @@ const Sentences = {}
  * @returns {Array} array of sentences
  */
 Sentences.extractAll = function (input) {
+  let sentences = []
+  let sentencesRaw = nlp(input).sentences().data()
+  sentencesRaw.forEach(function (s) {
+    sentences.push(s.text)
+  })
+  return sentences
+}
+
+// @deprecated. Not working well with sentences like : John X, M.D in VA.
+Sentences.extractAll_old = function (input) {
   const tokenizer = new natural.RegexpTokenizer({pattern: /[!?.]/})
   const sentences = tokenizer.tokenize(input)
   return sentences
